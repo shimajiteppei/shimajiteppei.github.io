cc13c8e90381f03aa60210394e57c5a87e6d67de
shimajiteppei <hotarutwitter@gmail.com>
2024-07-22 04:47:51 +0900

    performance improvement

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,28 +1,13 @@
 // https://github.com/squidfunk/mkdocs-material/issues/6196
 document$.subscribe(() => {
-    // 記事ページのHeadingに章番号を自動で振る
-    const isArticle = document.location.pathname.startsWith("/article");
-    if (isArticle) {
-        document
-            .querySelectorAll("h1, h2, h3, h4, h5, h6")
-            .forEach((it, index) => {
-                it.classList.add("article-page");
-            });
-    }
-
-    // 外部リンクを新しいタブで開く
-    document.querySelectorAll("a[href^='http']").forEach((it) => {
-        it.setAttribute("target", "_blank");
-        it.setAttribute("rel", "noopener noreferrer");
-    });
-
-    // テーブルソート https://github.com/tristen/tablesort
+    // テーブルソート
+    // https://github.com/tristen/tablesort
     document
         .querySelectorAll("article table:not(.highlight)")
         .forEach((it) => new Tablesort(it));
 
-    // katex
-    document.querySelectorAll(".language-math").forEach((it) => {
-        window.renderMathInElement(it, { ignoredTags: [] });
-    });
+    // Katexレンダリング
+    document
+        .querySelectorAll(".language-math")
+        .forEach((it) => renderMathInElement(it, { ignoredTags: [] }));
 });
---
+++ hooks/page_content.py
@@ -0,0 +1,92 @@
+import html.parser
+import re
+
+
+# HTML編集用の基底クラス
+# get_modified_html() で編集後のHTMLを取得できる
+# https://docs.python.org/3/library/html.parser.html#html.parser.HTMLParser
+class HTMLProcessor(html.parser.HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.modified_html = []
+
+    def handle_starttag(self, tag, attrs):
+        attrs = dict(attrs)
+        attrs = ' '.join(f'{key}="{value}"' for key, value in attrs.items())
+        self.modified_html.append(f'<{tag} {attrs}>')
+
+    def handle_endtag(self, tag):
+        self.modified_html.append(f'</{tag}>')
+
+    def handle_data(self, data):
+        self.modified_html.append(data)
+
+    def handle_entityref(self, name):
+        self.modified_html.append(f'&{name};')
+
+    def handle_charref(self, name):
+        self.modified_html.append(f'&#{name};')
+
+    def handle_comment(self, data):
+        self.modified_html.append(f'<!--{data}-->')
+
+    def handle_decl(self, decl):
+        self.modified_html.append(f'<!{decl}>')
+
+    def handle_pi(self, data):
+        self.modified_html.append(f'<?{data}>')
+
+    def get_modified_html(self):
+        return ''.join(self.modified_html)
+
+
+# 外部リンクに target="_blank" rel="noopener" を追加する
+class LinkProcessor(HTMLProcessor):
+    def __init__(self):
+        super().__init__()
+
+    def handle_starttag(self, tag, attrs):
+        attrs = dict(attrs)
+        if tag == 'a' and 'href' in attrs and re.match(r'^http', attrs['href']):
+            attrs['target'] = '_blank'
+            attrs['rel'] = 'noopener'
+
+        attrs = ' '.join(f'{key}="{value}"' for key, value in attrs.items())
+        self.modified_html.append(f'<{tag} {attrs}>')
+
+
+def add_target_blank(html_content):
+    parser = LinkProcessor()
+    parser.feed(html_content)
+    return parser.get_modified_html()
+
+
+# Headingに章番号用のクラスを自動で振る
+class HeadingProcessor(HTMLProcessor):
+    def __init__(self):
+        super().__init__()
+
+    def handle_starttag(self, tag, attrs):
+        attrs = dict(attrs)
+        if tag == 'h1' or tag == 'h2' or tag == 'h3' or tag == 'h4' or tag == 'h5' or tag == 'h6':
+            attrs['class'] = attrs.get('class', '') + ' article-page'
+
+        attrs = ' '.join(f'{key}="{value}"' for key, value in attrs.items())
+        self.modified_html.append(f'<{tag} {attrs}>')
+
+
+def add_article_class(html_content):
+    parser = HeadingProcessor()
+    parser.feed(html_content)
+    return parser.get_modified_html()
+
+
+# HTMLの内容を書き換えるフック
+# https://www.mkdocs.org/dev-guide/plugins/#on_page_content
+def on_page_content(html_content, page, config, files):
+    modified_html = add_target_blank(html_content)
+
+    if page.url.startswith('article'):
+        modified_html = add_article_class(modified_html)
+
+    return modified_html
