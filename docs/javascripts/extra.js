e5ec4c32a473a416fd2e3d268a4cf7ddb5231668
shimajiteppei <hotarutwitter@gmail.com>
2024-07-24 00:06:47 +0900

    remove web font

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,9 +1,3 @@
-WebFont.load({
-    google: {
-        families: ['M PLUS 1p:400,700', 'Roboto Mono']
-    }
-});
-
 // https://github.com/squidfunk/mkdocs-material/issues/6196
 document$.subscribe(() => {
     // テーブルソート
